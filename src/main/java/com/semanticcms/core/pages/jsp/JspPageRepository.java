e5f7a1a863949e77cec95c56e2c0416c397240e9
AO Industries, Inc <support@aoindustries.com>
2021-10-01 02:51:51 -0500

    Type-safe and scoped Servlet API attributes

--- src/main/java/com/semanticcms/core/pages/jsp/JspPageRepository.java
+++ src/main/java/com/semanticcms/core/pages/jsp/JspPageRepository.java
@@ -24,6 +24,7 @@ package com.semanticcms.core.pages.jsp;
 
 import com.aoapps.hodgepodge.util.Tuple2;
 import com.aoapps.net.Path;
+import com.aoapps.servlet.attribute.ScopeEE;
 import com.semanticcms.core.pages.local.LocalPageRepository;
 import java.io.IOException;
 import java.net.URL;
@@ -55,16 +56,11 @@ public class JspPageRepository extends LocalPageRepository {
 		}
 	}
 
-	private static final String INSTANCES_APPLICATION_ATTRIBUTE = JspPageRepository.class.getName() + ".instances";
+	private static final ScopeEE.Application.Attribute<ConcurrentMap<Path, JspPageRepository>> INSTANCES_APPLICATION_ATTRIBUTE =
+		ScopeEE.APPLICATION.attribute(JspPageRepository.class.getName() + ".instances");
 
 	private static ConcurrentMap<Path, JspPageRepository> getInstances(ServletContext servletContext) {
-		@SuppressWarnings("unchecked")
-		ConcurrentMap<Path, JspPageRepository> instances = (ConcurrentMap<Path, JspPageRepository>)servletContext.getAttribute(INSTANCES_APPLICATION_ATTRIBUTE);
-		if(instances == null) {
-			instances = new ConcurrentHashMap<>();
-			servletContext.setAttribute(INSTANCES_APPLICATION_ATTRIBUTE, instances);
-		}
-		return instances;
+		return INSTANCES_APPLICATION_ATTRIBUTE.context(servletContext).computeIfAbsent(__ -> new ConcurrentHashMap<>());
 	}
 
 	/**
